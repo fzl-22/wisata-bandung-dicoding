85ea15d018fd58bbd14001200c00f409afcdb345
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2023-09-15 22:00:33 +0700

    feat: codelab 2

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const DetailScreen(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: const DetailScreen(),
     );
   }
 }
